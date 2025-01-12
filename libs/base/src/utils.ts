6d9f282a00a665c821fb77e1ed876daa2b67e5ce
Szilard Hajba <szilu@symbion.hu>
2025-01-12 20:09:11 +0100

    F: str2color bugfix

--- libs/base/src/utils.ts
+++ libs/base/src/utils.ts
@@ -18,28 +18,30 @@ import { colord, extend } from 'colord'
 import lchPlugin from 'colord/plugins/lch'
 extend([lchPlugin])
 
-// Color
-export async function str2color(str: string, l: number = 40, c: number = 100, dark?: boolean): Promise<string> {
+export async function calcSha1Hex(str: string) {
 	// Generate SHA hash from str
 	const enc = new TextEncoder()
 	const data = enc.encode(str)
-	const hash = (await window.crypto.subtle.digest('sha-1', data)).toString()
+	return Array.from(new Uint8Array(await window.crypto.subtle.digest('sha-1', data))).map(byte => byte.toString(16).padStart(2, '0')).join('')
+}
+
+// Color
+export async function str2color(str: string, l: number = 40, c: number = 100, dark?: boolean): Promise<string> {
+	const hash = await calcSha1Hex(str)
 
 	// Generate color from hash
-	const color = colord({ l, c, h: parseInt(hash.slice(0, 3), 16) }).toHex()
+	const color = colord({ l: dark ? 100 - l : l, c, h: parseInt(hash.slice(0, 3), 16) }).toHex()
 
+	console.log('str2color', str, hash, color, parseInt(hash.slice(0, 3), 16))
 	return color
 }
 
 export async function str2colors(str: string, l: number = 40, c: number = 100, dark?: boolean): Promise<{ fg: string; bg: string }> {
-	// Generate SHA hash from str
-	const enc = new TextEncoder()
-	const data = enc.encode(str)
-	const hash = (await window.crypto.subtle.digest('sha-1', data)).toString()
+	const hash = await calcSha1Hex(str)
 
 	// Generate color from hash
-	const fg = colord({ l, c, h: parseInt(hash.slice(0, 3), 16) }).toHex()
-	const bg = colord({ l, c, h: parseInt(hash.slice(3, 6), 16) }).toHex()
+	const fg = colord({ l: dark ? 100 - l : l, c, h: parseInt(hash.slice(0, 3), 16) }).toHex()
+	const bg = colord({ l: dark ? 100 - l : l, c, h: parseInt(hash.slice(3, 6), 16) }).toHex()
 
 	return { fg, bg }
 }

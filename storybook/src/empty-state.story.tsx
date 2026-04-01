afa680590593f2e936207bf09d3d74006712494f
Szilárd Hajba <szilu@symbion.hu>
2026-04-01 09:58:10 +0200

    chore: replace verbose LGPL copyright headers with SPDX short identifiers

--- storybook/src/empty-state.story.tsx
+++ storybook/src/empty-state.story.tsx
@@ -1,19 +1,7 @@
+// SPDX-FileCopyrightText: Szilárd Hajba
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 import * as React from 'react'
-// This file is part of the Cloudillo Platform.
-// Copyright (C) 2024  Szilárd Hajba
-//
-// Cloudillo is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import { Story, Variant } from './storybook.js'
 import { EmptyState, Button } from '@cloudillo/react'
